cc8cd8f3ad2cd248707e3d073855f08591639c69
kakukie <luckiieo15@gmail.com>
2026-02-26 14:58:49 +0700

    fix(theme): prevent global theme from changing superadmin (forced dark) and landing page (forced light)

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,10 +2,24 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { usePathname } from "next/navigation"
 
 export function ThemeProvider({
     children,
     ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    const pathname = usePathname()
+    let forcedTheme: string | undefined = undefined
+
+    if (pathname === '/') {
+        forcedTheme = 'light'
+    } else if (pathname?.startsWith('/admin')) {
+        forcedTheme = 'dark'
+    }
+
+    return (
+        <NextThemesProvider {...props} forcedTheme={forcedTheme}>
+            {children}
+        </NextThemesProvider>
+    )
 }
